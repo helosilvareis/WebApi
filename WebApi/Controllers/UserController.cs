ab7fcf83f88acd7ef8b708404f63111283ff9198
Heloisa <Heloisa@DESKTOP-IEOQJP8>
2017-05-13 20:34:14 -0300

    Add project files.

---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("api/user")]
+    public class UserController : ApiController
+    {
+        private static List<UserModel> userList = new List<UserModel>();
+        
+        [AcceptVerbs("POST")]
+        [Route("CreateUser")]
+        public string CreateUser(UserModel user)
+        {
+            userList.Add(user);
+
+            return "User created!";
+        }
+
+        [AcceptVerbs("PUT")]
+        [Route("EditUser")]
+        public string EditUser(UserModel user)
+        {
+            userList.Where(_ => _.ID == user.ID)
+                         .Select(s =>
+                         {
+                             s.ID = user.ID;
+                             s.Login = user.Login;
+                             s.Name = user.Name;
+
+                             return s;
+
+                         }).ToList();
+
+            return "User changed!";
+        }
+
+        public string DeleteUser(int ID)
+        {
+            UserModel user = userList.Where(_ => _.ID == ID)
+                .Select(_ => _)
+                .First();
+
+            userList.Remove(user);
+
+            return "User deleted!";
+        }
+
+        public List<UserModel> GetUsers()
+        {
+            userList.Add(new UserModel { ID = 1, Login = "UserOne", Name = "User One" });
+            userList.Add(new UserModel { ID = 2, Login = "UserTwo", Name = "User Two" });
+            userList.Add(new UserModel { ID = 3, Login = "UserThree", Name = "User Three" });
+            return userList;
+        }
+    }
+}
---
+++ WebApi/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApi.Models
+{
+    public class UserModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+
+        public UserModel()
+        {
+
+        }
+
+        public UserModel(int ID, string Name, string Login)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Login = Login;
+        }
+    }
+}
